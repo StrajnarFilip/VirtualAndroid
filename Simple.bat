96428f44c1555ba0c4d69288b268b3b5d6585d06
Filip Strajnar <filip.strajnar@gmail.com>
2022-02-09 21:44:50 +0000

    batch.....

--- Simple.bat
+++ Simple.bat
@@ -1,1 +1,1 @@
-powershell -Command "Invoke-Expression (Invoke-WebRequest -Uri ""https://raw.githubusercontent.com/StrajnarFilip/VirtualAndroid/master/Create.ps1"")"
+powershell -Command "Invoke-Expression (Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/StrajnarFilip/VirtualAndroid/master/Create.ps1')"
