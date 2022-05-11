fed2a927f284ba52efc7365b60a4db31477d5572
StrajnarFilip <46705237+StrajnarFilip@users.noreply.github.com>
2022-05-11 22:43:20 +0200

    Update Create.ps1

--- Create.ps1
+++ Create.ps1
@@ -1,52 +1,69 @@
 <#
    Copyright 2022 Filip Strajnar
-
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-
        http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
 #>
+
 # Speed up download:
 $ProgressPreference = 'SilentlyContinue'
 
-# Download Java 8
-$java_zip = "java8.zip"
-$jdk_version_name = "jdk8u322-b06"
-Invoke-WebRequest -OutFile $java_zip -Uri "https://github.com/adoptium/temurin8-binaries/releases/download/${jdk_version_name}/OpenJDK8U-jdk_x64_windows_hotspot_8u322b06.zip"
+# Go to home directory:
+Set-Location ~
 
-# Unzip / Extract
-Expand-Archive -Path $java_zip -DestinationPath "java8"
-$jdk_full_path = "$(Get-Location)\java8\${jdk_version_name}"
+# Create directory that will contain everything
+New-Item -ItemType "directory" -Name "android-container"
 
-# Set java home
-[Environment]::SetEnvironmentVariable("JAVA_HOME", "${jdk_full_path}", [System.EnvironmentVariableTarget]::User)
-$env:JAVA_HOME = $jdk_full_path
+# Go inside the created directory
+Set-Location .\android-container
 
-# Set path to include java
-$old_user_path = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
-[Environment]::SetEnvironmentVariable("Path", "${old_user_path};${jdk_full_path}\bin", [System.EnvironmentVariableTarget]::User)
-$env:Path = [Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+# Set required environmental variables
+$env:ANDROID_HOME = "$(Get-Location)\.android"
+$env:ANDROID_SDK_ROOT = "$(Get-Location)\.android"
+$env:ANDROID_SDK_HOME = "$(Get-Location)"
+$env:ANDROID_USER_HOME = "$(Get-Location)\.android"
 
-# Download official CLI tools from google
+$env:ANDROID_EMULATOR_HOME = $env:ANDROID_USER_HOME
+$env:ANDROID_AVD_HOME = "${env:ANDROID_EMULATOR_HOME}/avd/"
+
+[System.Environment]::SetEnvironmentVariable("ANDROID_HOME", $env:ANDROID_HOME, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("ANDROID_SDK_ROOT", $env:ANDROID_SDK_ROOT, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("ANDROID_SDK_HOME", $env:ANDROID_SDK_HOME, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("ANDROID_USER_HOME", $env:ANDROID_USER_HOME, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("ANDROID_EMULATOR_HOME", $env:ANDROID_EMULATOR_HOME, [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("ANDROID_AVD_HOME", $env:ANDROID_AVD_HOME, [System.EnvironmentVariableTarget]::User)
+
+# Download and extract CLI tools
 $cli_tools = "cli_tools.zip"
 Invoke-WebRequest -OutFile $cli_tools -Uri "https://dl.google.com/android/repository/commandlinetools-win-8092744_latest.zip"
-# Extract .zip archive
-Expand-Archive -Path $cli_tools -DestinationPath ".\android\cmdline-tools"
-# Set environmental variable
-$env:ANDROID_SDK_ROOT = "$(Get-Location)\android"
-[Environment]::SetEnvironmentVariable("ANDROID_SDK_ROOT", $env:ANDROID_SDK_ROOT, [System.EnvironmentVariableTarget]::User)
+Expand-Archive -Path $cli_tools -DestinationPath ".\.android\cmdline-tools"
+Remove-Item $cli_tools
 
-Set-Location .\android
+Set-Location .\.android
+
+# Create required directories
 New-Item -Type "directory" -Path .\platforms
+New-Item -Type "directory" -Path .\platform-tools
+
+# Add CLI tools to path
+$BaseAndroidPath=$(Get-Location)
+[System.Environment]::SetEnvironmentVariable(
+"Path",
+"${BaseAndroidPath}\platform-tools;${BaseAndroidPath}\cmdline-tools\latest\bin;$([System.Environment]::GetEnvironmentVariable("Path",[System.EnvironmentVariableTarget]::User))",
+[System.EnvironmentVariableTarget]::User)
+
+# Fix the name to latest
 Set-Location .\cmdline-tools
 Rename-Item -Path .\cmdline-tools -NewName "latest"
+
+# Install basic tools
+'y', 'y', 'y', 'y' | .\latest\bin\sdkmanager.bat --install "platform-tools"
 'y', 'y', 'y', 'y' | .\latest\bin\sdkmanager.bat --install "build-tools;32.0.0"
 'y', 'y', 'y', 'y' | .\latest\bin\sdkmanager.bat --install "system-images;android-32;google_apis_playstore;x86_64"
 .\latest\bin\avdmanager.bat create avd --name "Machine1" --device "28" --package "system-images;android-32;google_apis_playstore;x86_64"
